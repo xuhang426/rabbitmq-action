b329e77fa96d772563c55c479eb243c85cd7ef34
xuhang426 <123456>
2020-10-23 08:32:55 +0800

    123

---
+++ src/main/java/com/example/rabbitmqaction/RabbitmqActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqActionApplication.class, args);
+    }
+
+}
